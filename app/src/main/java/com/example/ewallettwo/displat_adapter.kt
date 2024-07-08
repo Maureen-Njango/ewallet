25806403e97800c52630598441ab1de54b616e10
Maureen-Njango <maureennjangombira@gmail.com>
2024-07-08 09:10:27 +0300

    changes

---
+++ app/src/main/java/com/example/ewallettwo/displat_adapter.kt
@@ -0,0 +1,14 @@
+package com.example.ewallettwo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class displat_adapter(var ) {
+}
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/ewallettwo/wallet.kt
@@ -0,0 +1,12 @@
+package com.example.ewallettwo
+
+data class wallet(
+    var imageone:String,
+    var imagetwo:String,
+    var name:String,
+    var salary:String,
+
+
+)
+
+
