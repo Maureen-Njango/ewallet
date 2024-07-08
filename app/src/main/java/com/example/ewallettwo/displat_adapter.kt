cc107b62b85533664594218f35833ea9b8de12e0
Maureen-Njango <maureennjangombira@gmail.com>
2024-07-08 09:32:01 +0300

    changes

--- app/src/main/java/com/example/ewallettwo/displat_adapter.kt
+++ app/src/main/java/com/example/ewallettwo/displat_adapter.kt
@@ -4,8 +4,48 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class displat_adapter(var ) {
+package com.example.newsinformation
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(var wallet:List<wallet>): RecyclerView.Adapter<WalletInformation>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WalletInformation{
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.items_list, parent,false)
+        return WalletInformation(itemView)
+
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return wallet.size
+
+
+    }
 }
+class WalletInformation(itemView:View):RecyclerView.ViewHolder(itemView){
+    var textimage =itemView.findViewById<TextView>(R.id.tvout)
+    var texttitle = itemView.findViewById<TextView>(R.id.tvmoney)
+    var textDate = itemView.findViewById<TextView>(R.id.tvbalance)
+    var authorsname = itemView.findViewById<TextView>(R.id.tvout)
+    var preview = itemView.findViewById<TextView>(R.id.tvThree)
+    var seemore = itemView.findViewById<TextView>(R.id.tvFour)
+
+
+
+
+
+
+
+}
+
+
 
 
 
